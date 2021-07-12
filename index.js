b3207f9b884ede508fc6bc39ade7af9f6129e96b
Amanda KIBENDE <contactproamanda@gmail.com>
2021-07-12 15:51:59 +0200

    necessary variables

---
+++ index.js
@@ -0,0 +1,92 @@
+const main = document.getElementsByClassName("main")
+const number =[1, 2, 3, 4, 5]
+const dataQuizz = [
+    {
+     question : "In what food Rick transform himself to escape the family therapy session ?",
+     answers : ["Carrot", "Apple", "Pickle"],
+     result : 2
+},
+    
+    {
+    question : "How many grand kids Rick have ?", 
+     answers : [3, 4, 2],
+     result : 2
+},
+    {
+     question : "Who is Morty high school crush ?", 
+     answers : ["Jessica", "Lina ", "Emily"],
+     result : 0
+},
+    {
+     question : "What is Rick lastname ?",
+     answers : ["Bond", "Sanchez", "Smith"],
+     result : 1
+},
+    {
+     question : "Who are Morty's parents?", 
+     answers : ["Summer and Dylan", "Ben and Jerry", "Beth and Jerry"],
+     result : 2
+}
+
+]
+
+
+
+function data (main){
+    function displayQuest(){
+        
+    }
+}
+
+
+
+
+
+
+
+// find the correct answers at the end of the game it display the numbers of good answers.
+// If all the answers are correct display "shabala wub dub dub, you are an expert" else diplay "meh, you are not ready to squash yet"
+
+// Numbers of questions left, question and answers should appear each time a button is pressed.
+
+
+
+// Questions, answers, result are linked, since they are linked questions and object should be in the same array
+
+// Display the questions.
+// each questions will be already write.
+// they will be inside an object,
+// each time a btn is clicked the questions should appear on the paragraph
+
+// Display answers
+// I would have an object with answers inside.
+// each time a btn is clicked the answers should display in a random btn 
+
+
+// I need to select what's inside class main in order to input the values of my variables.
+
+
+// Display number of questions left
+// I should have an array of numbers.
+// each time a btn is clicked the number should appear on class=questions and display "Question [1] out 5"
+// 
+//
+
+// when there is no more questions display the result.
+
+// At the end display the nunmber of correct answers
+// count the number of correct answer.
+// every time an answer is true we will add 1 in order to have the result.
+
+
+// I will take all the good answers and return a number of the size of the answers
+// if the number of good answers are equal to 5 return "shabala wub dub dub, you are an expert" else "meh, you are not ready to squash yet"
+//
+//
+//
+// each time a button is clicked it should display a new array of data. 
+
+
+
+
+
