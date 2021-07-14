f10d7193ca4f17db1a1c47acea181c78e02b1eef
Amanda KIBENDE <contactproamanda@gmail.com>
2021-07-14 12:27:57 +0200

    answers display

--- index.js
+++ index.js
@@ -1,147 +1,122 @@
-const btn = document.querySelectorAll(".btn-choice")
+const btn = document.querySelectorAll(".btn-choice");
 const quizzIndex = 0;
 
-
 const dataQuizz = [
-    {
-     question : "In what food Rick transform himself to escape the family therapy session ?",
-     answers : ["Carrot", "Apple", "Pickle"],
-     result : 2
-},
-    {
-    question : "How many grand kids Rick have ?", 
-     answers : [3, 4, 2],
-     result : 2
-},
-    {
-     question : "Who is Morty high school crush ?", 
-     answers : ["Jessica", "Lina ", "Emily"],
-     result : 0
-},
-    {
-     question : "What is Rick lastname ?",
-     answers : ["Bond", "Sanchez", "Smith"],
-     result : 1
-},
-    {
-     question : "Who are Morty's parents?", 
-     answers : ["Summer and Dylan", "Ben and Jerry", "Beth and Jerry"],
-     result : 2
-}
-
-]
-
-
+  {
+    question:
+      "In what food Rick transform himself to escape the family therapy session ?",
+    answers: ["Carrot", "Apple", "Pickle"],
+    result: 2,
+  },
+  {
+    question: "How many grand kids Rick have ?",
+    answers: [3, 4, 2],
+    result: 2,
+  },
+  {
+    question: "Who is Morty high school crush ?",
+    answers: ["Jessica", "Lina ", "Emily"],
+    result: 0,
+  },
+  {
+    question: "What is Rick lastname ?",
+    answers: ["Bond", "Sanchez", "Smith"],
+    result: 1,
+  },
+  {
+    question: "Who are Morty's parents?",
+    answers: ["Summer and Dylan", "Ben and Jerry", "Beth and Jerry"],
+    result: 2,
+  },
+];
 
 // Display numbers of questions
 //  Each time a btn is clicked display the number inside the array until the end of that array.
-
-
 
 // find the correct answers at the end of the game it display the numbers of good answers.
 // If all the answers are correct display "shabala wub dub dub, you are an expert" else diplay "meh, you are not ready to squash yet"
 
 // Numbers of questions left, question and answers should appear each time a button is pressed.
 
-
-
 // Questions, answers, result are linked, since they are linked questions and object should be in the same array
 
+function displayQuizz(quizzIndex) {
+  const currentQuestion = dataQuizz[quizzIndex];
 
-function displayQuizz(quizzIndex){
+  // Display the current question n° on the page  (you can use the quizzIndex);
 
-  
-    const currentQuestion = dataQuizz[quizzIndex];
+  const title = document.getElementById("current-quest");
+  title.textContent = quizzIndex += 1;
 
-    console.log(currentQuestion);
+  // Display the question on the page
 
-    // Display the current question n° on the page  (you can use the quizzIndex);
+  const quest = document.querySelector(".questions");
+  quest.innerHTML = currentQuestion.question;
 
-     const title = document.getElementById("current-quest");
+  // Zone containing the answer buttons
+  const divBtn = document.querySelector(".btn-main");
+  divBtn.innerHTML = "";
+
+  currentQuestion.answers.forEach((answer) => {
+    // create a dom element
+    const createBtn = document.createElement("button");
+    // I inject the dom element "create btn" inside the divBtn
+    createBtn.innerHTML = answer;
+    createBtn.classList.add("btn-choice");
+    divBtn.appendChild(createBtn);
+    console.log(createBtn);
+  });
 
 
-      title.textContent += 1;
-    
-    //   console.log(title);
-
-    // Display the question on the page
-
-     console.log(dataQuizz[0].question);
-     const quest = document.querySelector(".questions");
-
-     quest.innerHTML = dataQuizz[0].question;
-
-    //  console.log(quest);
-
-
-     // In every button insert the choice text
-
-    //   console.log((dataQuizz[i].answers));
-
-      const btnChoice = document.querySelectorAll(".btn-choice")
-        
-       console.log((dataQuizz[0].answers));
-
-    for (let i = 0; i < dataQuizz[i].answers.length; index++) {
-
-    const element = dataQuizz[i].answers.length;
-    
-}
-
-      response.forEach((answer) =>{
-       
-              // selection a dom element with class.btn-main
-      const  divBtn =  document.querySelector(".btn-main");
-      
-      // create a dom element
-      const createBtn = document.createElement("button");
-     
-
-      // I inject the dom element "create btn" inside the divBtn
-      createBtn.innerHTML = answer.answers;
-
-      
-      createBtn.classList.add("btn-choice") 
-    
-      divBtn.appendChild(createBtn);
-
+    const btn = document.querySelector.forEach(createBtn => {
+    createBtn.addEventListener("click", function(index){
+        console.log(index); 
     });
+    nextQuest();
+});
+
 
 
 }
 
 
-const response=[...dataQuizz];
+
+  function nextQuest(){
+
+    quizzIndex += 1;
+
+    if (quizzIndex < dataQuizz.length){
+        displayQuizz(dataQuizz[quizzIndex]);
+    } else {
+        gameOver();
+    };
+  };
 
 
-console.log(displayQuizz());
+displayQuizz(0);
 
+// each time the answer is correct add 1, at the end of the game display the total
 
+// each questions will be already write.
+// they will be inside an object,
+// each time a btn is clicked the questions should appear on the paragraph
 
+// Select every button, for every button attach an event handler that will look
+// if the current button is the correct answer.
 
-// each time the answer is correct add 1, at the end of the game display the total 
+// when there is no more questions display the result.
+// At the end display the nunmber of correct answers
+// count the number of correct answer.
+// every time an answer is true we will add 1 in order to have the result.
 
-    // each questions will be already write.
-    // they will be inside an object,
-    // each time a btn is clicked the questions should appear on the paragraph
+// I will take all the good answers and return a number of the size of the answers
+// if the number of good answers are equal to 5 return "shabala wub dub dub, you are an expert" else "meh, you are not ready to squash yet"
 
-    // Select every button, for every button attach an event handler that will look
-    // if the current button is the correct answer.
+function gameOver(){
 
-    // when there is no more questions display the result. 
-    // At the end display the nunmber of correct answers
-    // count the number of correct answer.
-    // every time an answer is true we will add 1 in order to have the result.
-
-
-    // I will take all the good answers and return a number of the size of the answers
-    // if the number of good answers are equal to 5 return "shabala wub dub dub, you are an expert" else "meh, you are not ready to squash yet"
-
-// function gameOver(){
-
-//     function result (){
-//         total=0;
-//         i=0;
+    function result (){
+        total=0;
+        i=0;
 
 //    if (dataQuizz[i].result[index] === true){
 //        return total++}
@@ -150,34 +125,28 @@ console.log(displayQuizz());
 //             return total ++
 //         }
 
-//   ou     
-  
-//   for (total =0; dataQuizz[i].result[index]  === true; total++);
-//     };
+//   ou
 
+  for (total =0; dataQuizz[i].result[index]  === true; total++);
+    };
 
-//     console.log(result);
+    console.log(result);
 
-    
+    const total = dataQuizz.length >= 5;
 
-//     const total = dataQuizz.length >= 5;
-
-//     if (total === 5){
-//         return 'your result is : ${}, Shabala wub dub dub, you are an expert'
-//     } else { 
-//         return 'your result is : ${}, Meh, you are not ready to squash yet'
-//     }
-// };
+    if (total === 5){
+        return 'your result is : ${total}, Shabala wub dub dub, you are an expert'
+    } else {
+        return 'your result is : ${total}, Meh, you are not ready to squash yet'
+    }
+};
 
 //displayQuizz(quizzIndex)
 
-
 // Display answers
 // I would have an object with answers inside.
-// each time a btn is clicked the answers should display in a random btn 
-
+// each time a btn is clicked the answers should display in a random btn
 
 // I need to select what's inside class main in order to input the values of my variables.
-
 
 // each time a button is clicked it should display a new array of data
