32c4002244435e4a456b7c8b8f79d599faaec15e
Amanda KIBENDE <contactproamanda@gmail.com>
2021-07-14 20:51:49 +0200

    stock for displaying result

--- index.js
+++ index.js
@@ -30,26 +30,24 @@ const dataQuizz = [
   },
 ];
 
-// Display numbers of questions
-//  Each time a btn is clicked display the number inside the array until the end of that array.
-
-// find the correct answers at the end of the game it display the numbers of good answers.
-// If all the answers are correct display "shabala wub dub dub, you are an expert" else diplay "meh, you are not ready to squash yet"
-
-// Numbers of questions left, question and answers should appear each time a button is pressed.
-
-// Questions, answers, result are linked, since they are linked questions and object should be in the same array
 
 function displayQuizz(quizzIndex) {
+
   const currentQuestion = dataQuizz[quizzIndex];
+  const total = 0; 
 
   // Display the current question n° on the page  (you can use the quizzIndex);
 
   const title = document.getElementById("current-quest");
   title.textContent = quizzIndex += 1;
 
-  // Display the question on the page
+// Diqplay the length of questions
 
+  const questLength = document.getElementById("quest-limit");
+  questLength.textContent = dataQuizz.length;
+
+  // Display the question on the page
+  
   const quest = document.querySelector(".questions");
   quest.innerHTML = currentQuestion.question;
 
@@ -64,89 +62,96 @@ function displayQuizz(quizzIndex) {
     createBtn.innerHTML = answer;
     createBtn.classList.add("btn-choice");
     divBtn.appendChild(createBtn);
-    console.log(createBtn);
   });
 
+// each time a button is pressed it add 1 index to my displayQuizz
+   const btnClick = document.querySelectorAll(".btn-choice").forEach(button=> {
+    button.addEventListener("click", function(){
 
-    const btn = document.querySelector.forEach(createBtn => {
-    createBtn.addEventListener("click", function(index){
-        console.log(index); 
-    });
-    nextQuest();
-});
 
+        // for (total = 0; currentQuestion.result === true; total++); {
+        //     const result = document.querySelector(".total")
+        //     result.innerHTML =  total; 
+        // }
+    
+
+        nextQuest();
+
+       });
+   });
+
+
+   function nextQuest(){
+
+    quizzIndex + 1;
+    
+    if (quizzIndex < dataQuizz.length){
+        displayQuizz(quizzIndex);
+    } else {
+        gameOver();
+    };
+  }; 
+
+
+  function gameOver(){
+
+    
+
+    const endGame = document.querySelector(".remove-at-the-end")
+    endGame.innerHTML = " "
+
+
+    const titleOver = document.querySelector(".title")
+    titleOver.innerHTML = "GAME OVER GLIP GLOPS !"
+
+
+    // if (total === dataQuizz.length ){
+    //     return 'your result is : ${total}, Shabala wub dub dub, you are an expert'
+    // } else {
+    //     return 'your result is : ${total}, Meh, you are not ready to squash yet'
+    // }
+
+
+ };
+
+ 
 
 
 }
 
 
 
-  function nextQuest(){
-
-    quizzIndex += 1;
-
-    if (quizzIndex < dataQuizz.length){
-        displayQuizz(dataQuizz[quizzIndex]);
-    } else {
-        gameOver();
-    };
-  };
-
-
 displayQuizz(0);
 
-// each time the answer is correct add 1, at the end of the game display the total
+// function gameOver(){
 
-// each questions will be already write.
-// they will be inside an object,
-// each time a btn is clicked the questions should appear on the paragraph
+//     function result (){
+//         total=0;
+//         i=0;
 
-// Select every button, for every button attach an event handler that will look
-// if the current button is the correct answer.
+// //    if (dataQuizz[i].result[index] === true){
+// //        return total++}
+// // ou
+// //         while (dataQuizz[i].result[index]  === true){
+// //             return total ++
+// //         }
 
-// when there is no more questions display the result.
-// At the end display the nunmber of correct answers
-// count the number of correct answer.
-// every time an answer is true we will add 1 in order to have the result.
+// //   ou
 
-// I will take all the good answers and return a number of the size of the answers
-// if the number of good answers are equal to 5 return "shabala wub dub dub, you are an expert" else "meh, you are not ready to squash yet"
+  
 
-function gameOver(){
+//     console.log(result);
 
-    function result (){
-        total=0;
-        i=0;
+//     const total = dataQuizz.length >= 5;
 
-//    if (dataQuizz[i].result[index] === true){
-//        return total++}
-// ou
-//         while (dataQuizz[i].result[index]  === true){
-//             return total ++
-//         }
+//     if (total === dataQuizz.length ){
+//         return 'your result is : ${total}, Shabala wub dub dub, you are an expert'
+//     } else {
+//         return 'your result is : ${total}, Meh, you are not ready to squash yet'
+//     }
+// };
 
-//   ou
 
-  for (total =0; dataQuizz[i].result[index]  === true; total++);
-    };
 
-    console.log(result);
 
-    const total = dataQuizz.length >= 5;
 
-    if (total === 5){
-        return 'your result is : ${total}, Shabala wub dub dub, you are an expert'
-    } else {
-        return 'your result is : ${total}, Meh, you are not ready to squash yet'
-    }
-};
-
-//displayQuizz(quizzIndex)
-
-// Display answers
-// I would have an object with answers inside.
-// each time a btn is clicked the answers should display in a random btn
-
-// I need to select what's inside class main in order to input the values of my variables.
-
-// each time a button is clicked it should display a new array of data
